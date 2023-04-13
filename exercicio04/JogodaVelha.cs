3a2085e83caa6193e4e724141c6e10854719375b
markesdl <114708760+mrkaka1@users.noreply.github.com>
2023-04-12 23:28:45 -0400

    Adicionar arquivos de projeto.

---
+++ exercicio04/JogodaVelha.cs
@@ -0,0 +1,139 @@
+﻿public class JogodeVelha
+{
+    private string[,] grade = new string[3,3];
+    public string vazia = "   ";
+    private string jogador1 = " X ";
+    private string jogador2 = " O ";
+
+    public JogodeVelha() 
+    {
+        for(int i = 0; i < grade.GetLength(0); i++)
+        {
+            for(int j = 0; j < grade.GetLength(1); j++)
+            {
+                grade[i, j]= vazia;
+            }
+        }
+    }
+    public void Grade()
+    {
+        Console.WriteLine($"    0   1   2 "); 
+        Console.WriteLine($" 0|{grade[0, 0]}|{grade[0, 1]}|{grade[0,2]}|");
+        Console.WriteLine(" ---------------");
+        Console.WriteLine($" 1|{grade[1, 0]}|{grade[1, 1]}|{grade[1,2]}|");
+        Console.WriteLine(" ---------------");
+        Console.WriteLine($" 2|{grade[2, 0]}|{grade[2, 1]}|{grade[2,2]}|");
+        
+    } 
+
+    public bool Verificar()
+    {
+        if ((grade[0,0] == jogador1 && grade[0,1] == jogador1 && grade[0,2] == jogador1 || 
+            grade[0,0] == jogador2 && grade[0,1] == jogador2 && grade[0,2] == jogador2)) 
+        {
+            return true;}
+
+        if ((grade[1, 0] == jogador1 && grade[1, 1] == jogador1 && grade[1, 2] == jogador1 ||
+            grade[1, 0] == jogador2 && grade[1, 1] == jogador2 && grade[1, 2] == jogador2))
+        { 
+            return true; }
+
+        if ((grade[2, 0] == jogador1 && grade[2, 1] == jogador1 && grade[2, 2] == jogador1 ||
+            grade[2, 0] == jogador2 && grade[2, 1] == jogador2 && grade[2, 2] == jogador2)) 
+        {
+            return true; }
+
+        //----------------------------------------------------------------------------------------------------
+
+        if ((grade[0, 0] == jogador1 && grade[1, 0] == jogador1 && grade[2, 0] == jogador1 ||
+            grade[0, 0] == jogador2 && grade[1, 0] == jogador2 && grade[2, 0] == jogador2)) 
+        { 
+            return true; }
+
+        if ((grade[0, 1] == jogador1 && grade[1, 1] == jogador1 && grade[2, 1] == jogador1 ||
+            grade[0, 1] == jogador2 && grade[1, 1] == jogador2 && grade[2, 1] == jogador2)) 
+        { 
+            return true; }
+
+        if ((grade[0, 2] == jogador1 && grade[1, 2] == jogador1 && grade[2, 2] == jogador1 ||
+            grade[0, 2] == jogador2 && grade[1, 2] == jogador2 && grade[2, 2] == jogador2)) 
+        { 
+            return true; }
+
+        //--------------------------------------------------------------------------------------------
+
+        if ((grade[0,0] == jogador1 && grade[1,1] == jogador1 && grade[2,2] == jogador1 ||
+            grade[0, 0] == jogador2 && grade[1, 1] == jogador2 && grade[2, 2] == jogador2))
+        { 
+            return true;}
+
+        if ((grade[2,0] == jogador1 && grade[2,1] == jogador1 && grade[0,2] == jogador1 ||
+            grade[2, 0] == jogador1 && grade[2, 1] == jogador1 && grade[0, 2] == jogador1))
+        {
+            return true;}
+
+        else
+        {
+            bool empata = true;
+            int partida = 0;
+            for(int i = 0; i< grade.GetLength(0); i++)
+            {
+                for(int j = 0; j< grade.GetLength(1); j++)
+                {
+                    if (grade[i, j] == vazia)
+                    {
+                        empata = false;
+                        partida++;
+                    }
+                    
+                }
+            }
+            if (partida == 0)
+            {
+                return true;
+            }
+            return empata;
+        }
+        
+    }
+
+    public bool SetJogada1(int linha, int coluna)
+    {
+        if (linha >= 0 && linha <= 2 && coluna >= 0 && coluna <= 2) 
+        {
+            if (grade[linha, coluna] == vazia)
+            {
+                grade[linha, coluna] = jogador1;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool SetJogada2(int linha, int coluna)
+    {
+        if (linha >= 0 && linha <= 2 && coluna >= 0 && coluna <= 2)
+        {
+            if (grade[linha, coluna] == vazia)
+            {
+                grade[linha, coluna] = jogador2;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ exercicio04/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+class Exercicio
+{
+    static void Main(string[] args)
+    {
+    inicio:;
+        JogodeVelha game = new JogodeVelha();
+        while (true)
+        {
+        erro:;
+            game.Grade();
+            Console.WriteLine("Jogador 1");
+            Console.WriteLine("Digite a coluna:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a linha:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (game.SetJogada1(b, a))
+                /*if (game.SetJogada1(b, a))
+                {
+                    Console.WriteLine("Jogada Inválida.");
+                    Console.ReadKey();
+                    goto erro;
+                }*/
+            
+            if (game.Verificar())
+            {
+                    while (true)
+                    {
+                        Console.Clear();
+                        game.Grade();
+                        Console.WriteLine("Acabou");
+                        Console.WriteLine("Deseja iniciar novamente: " + "\n1 - Sim"+"\n2 - Não");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        if(num == 1)
+                        {
+                            goto inicio;
+                        }
+                        if (num == 2)
+                        {
+                            goto sair;
+                        }
+                        if(num != 1 && num != 2)
+                        {
+                            Console.Write("Opção inválida!");
+                            Console.ReadLine();
+                        }
+                    }                   
+            }
+        erro2:;
+            Console.Clear();
+            game.Grade();
+            Console.WriteLine("Jogador 2");
+            Console.WriteLine("Digite a coluna:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a linha:");
+            int d = Convert.ToInt32(Console.ReadLine());
+            if (game.SetJogada2(c, d)) ;
+               /* if (!game.SetJogada2(c, d))
+                {
+                    Console.WriteLine("Jogada Inválida.");
+                    Console.ReadKey();
+                    goto erro2;
+                }*/
+        }
+        if (game.Verificar())
+        {
+            while (true)
+            {
+                Console.Clear();
+                game.Grade();
+                Console.WriteLine("Acabou");
+                Console.WriteLine("Deseja iniciar novamente: " + "\n1 - Sim" + "\n2 - Não");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num == 1)
+                {
+                    goto inicio;
+                }
+                if (num == 2)
+                {
+                    goto sair;
+                }
+                if (num != 1 && num != 2)
+                {
+                    Console.Write("Opção inválida!");
+                    Console.ReadLine();
+                }
+            }
+        }
+    sair:;
+
+        Console.ReadLine();
+    }
+}
